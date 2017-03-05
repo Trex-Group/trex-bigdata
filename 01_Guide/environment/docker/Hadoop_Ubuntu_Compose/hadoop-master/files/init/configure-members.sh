48c434ce65654adf1c7763775fe24caeb180eff9
xenron <xenron@hotmail.com>
2017-03-06 07:27:41 +0900

    msg

--- 01_Guide/environment/docker/Hadoop_Ubuntu_Compose/hadoop-master/files/init/configure-members.sh
+++ 01_Guide/environment/docker/Hadoop_Ubuntu_Compose/hadoop-master/files/init/configure-members.sh
@@ -5,12 +5,11 @@ rm -rf /opt/hadoop/etc/hadoop/slaves
 [[ ! -z $HADOOP_SLAVE ]] && for slave in $HADOOP_SLAVE; do echo $slave >> /opt/hadoop/etc/hadoop/slaves; done
 
 function init_members(){
-        while read -r member
-        do
-                scp /opt/hadoop/etc/hadoop/slaves $member:$HADOOP_CONF_DIR/slaves #hadoop
-        done < "$HADOOP_SLAVE"
+    for member in $HADOOP_SLAVE
+    do
+        scp /opt/hadoop/etc/hadoop/slaves $member:$HADOOP_CONF_DIR/slaves #hadoop
+    done
 }
-
 
 init_members
 
