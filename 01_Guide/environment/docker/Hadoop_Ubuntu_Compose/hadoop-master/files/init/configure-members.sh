f82c075f5473fca7416aedeeb2f392ca0231b6c7
xenron <xenron@hotmail.com>
2017-03-06 06:44:19 +0900

    msg

--- 01_Guide/environment/docker/Hadoop_Ubuntu_Compose/hadoop-master/files/init/configure-members.sh
+++ 01_Guide/environment/docker/Hadoop_Ubuntu_Compose/hadoop-master/files/init/configure-members.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-slaves=/tmp/slaves
-rm -f $slaves
->$slaves
-
+echo $HADOOP_MASTER > /opt/hadoop/etc/hadoop/master
+rm -rf /opt/hadoop/etc/hadoop/slaves
+[[ ! -z $HADOOP_SLAVE ]] && for slave in $HADOOP_SLAVE; do echo $slave >> /opt/hadoop/etc/hadoop/slaves; done
 
 function init_members(){
-        members=$(serf members 2>&1| tac)
-        while read -r line; do
-                if [[ ($line =~ "slave" || $line =~ "master") && $line =~ "alive" ]]
-                then
-                        alive_mem=$(echo $line | cut -d " " -f 1 2>&1) #get hosts
-                        echo "$alive_mem">>$slaves
-                        continue
-                fi
-        done <<< "$members"
-        #copy slave file to all slaves and master
-        #create hbase 
-        members_line=$(paste -d, -s $slaves 2>&1)
-        memstr='members' #uniq string for replace
-
         while read -r member
         do
-                scp $slaves $member:$HADOOP_CONF_DIR/slaves #hadoop
-        done < "$slaves"
+                scp /opt/hadoop/etc/hadoop/slaves $member:$HADOOP_CONF_DIR/slaves #hadoop
+        done < "$HADOOP_SLAVE"
 }
 
 
--- 01_Guide/environment/docker/Hadoop_Ubuntu_Compose/hadoop-master/files/init/start-hadoop.sh
+++ 01_Guide/environment/docker/Hadoop_Ubuntu_Compose/hadoop-master/files/init/start-hadoop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo $HADOOP_MASTER > /opt/hadoop/etc/hadoop/slaves
-[[ ! -z $HADOOP_SLAVE ]] && for slave in $HADOOP_SLAVE; do echo $slave > /opt/hadoop/etc/hadoop/slaves; done
-
 $HADOOP_INSTALL/sbin/start-dfs.sh
 
 echo -e "\n"
