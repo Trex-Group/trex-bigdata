8d1da01d9a71946a2048c9a52764835ce2a3f5a6
xenron <xenron@hotmail.com>
2017-03-29 07:22:06 +0900

    msg

--- 01_Guide/environment/docker/Hadoop_Ubuntu_Compose/hbase-master/files/hadoop/configure-members.sh
+++ 01_Guide/environment/docker/Hadoop_Ubuntu_Compose/hbase-master/files/hadoop/configure-members.sh
@@ -3,9 +3,9 @@ echo $HADOOP_MASTER > /opt/hadoop/etc/hadoop/master
 rm -rf /opt/hadoop/etc/hadoop/slaves
 [[ ! -z $HADOOP_SLAVE ]] && for slave in $HADOOP_SLAVE; do echo $slave >> /opt/hadoop/etc/hadoop/slaves; done
 
-members_line=$(paste -d, -s $HADOOP_SLAVE 2>&1)
+# members_line=$(paste -d, -s $HADOOP_SLAVE 2>&1)
 memstr='members' #uniq string for replace
-sed -i -e "s/$memstr/$members_line/g" /opt/hbase/conf/hbase-site.xml
+sed -i -e "s/$memstr/$ZOOKEEPER_LIST/g" /opt/hbase/conf/hbase-site.xml
 
 function init_members(){
     for member in $HADOOP_SLAVE
